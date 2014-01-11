98446c5878e735bfeda4400c8852d9ef63d045d2
Colin Graf <colin.graf@sovereign-labs.com>
2014-01-11 10:25:29 +0100

    Console: Add stderr print functions

--- include/nstd/Console.h
+++ include/nstd/Console.h
@@ -8,4 +8,6 @@ class Console
 public:
   static int_t print(const tchar_t* str);
   static int_t printf(const tchar_t* format, ...);
+  static int_t error(const tchar_t* str);
+  static int_t errorf(const tchar_t* format, ...);
 };
--- src/Console.cpp
+++ src/Console.cpp
@@ -28,3 +28,25 @@ int_t Console::printf(const tchar_t* format, ...)
   va_end(ap);
   return result;
 }
+
+int_t Console::error(const tchar_t* str)
+{
+#ifdef _MSC_VER
+  return _fputts(str, stderr);
+#else
+  return fputs(str, stderr);
+#endif
+}
+
+int_t Console::errorf(const tchar_t* format, ...)
+{
+  va_list ap;
+  va_start(ap, format);
+#ifdef _UNICODE
+  int_t result = vfwprintf(stderr, format, ap);
+#else
+  int_t result = vfprintf(stderr, format, ap);
+#endif
+  va_end(ap);
+  return result;
+}
