b057497f01580504f2b0775af6a69ac3751a7a56
Colin Graf <colin.graf@sovereign-labs.com>
2014-01-12 23:02:33 +0100

    Time: Add some comments

--- include/nstd/Time.h
+++ include/nstd/Time.h
@@ -7,6 +7,13 @@
 class Time
 {
 public:
+  /** Retrieve local system time. The function returns the local system time in milliseconds since 1 January 1970 (Unix time with millisesond precision).
+  * @return The current local system time (in millisconds).
+  */
   static timestamp_t time();
+
+  /** Retrieve ticks (in milliseconds) that have passed since the system was started.
+  * @return The ticks (in millisconds) that have currently passed since the system was started.
+  */
   static timestamp_t ticks();
 };
