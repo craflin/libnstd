c65a3976540f17dec3432b19cb9d7122015591c1
Colin Graf <colin.graf@sovereign-labs.com>
2014-01-19 18:05:58 +0100

    Debug: Fix assert not working properly in UNICODE builds

--- include/nstd/Debug.h
+++ include/nstd/Debug.h
@@ -23,6 +23,11 @@ void __cdecl __debugbreak(void);
 #define ASSERT(exp) ((void_t)1)
 #define VERIFY(exp) ((void_t)(exp))
 #else
+#ifdef _MSC_VER
+#define ASSERT(exp) ((void_t)(!(exp) && Debug::printf(_T("%s(%u): assertion failed: %s\n"), __TFILE__, __LINE__, _T(#exp)) && (HALT(), 1)))
+#define VERIFY(exp) ((void_t)(!(exp) && Debug::printf(_T("%s(%u): verification failed: %s\n"), __TFILE__, __LINE__, _T(#exp)) && (HALT(), 1)))
+#else
 #define ASSERT(exp) ((void_t)(!(exp) && Debug::printf(_T("%s:%u: assertion failed: %s\n"), __TFILE__, __LINE__, #exp) && (HALT(), 1)))
 #define VERIFY(exp) ((void_t)(!(exp) && Debug::printf(_T("%s:%u: verification failed: %s\n"), __TFILE__, __LINE__, #exp) && (HALT(), 1)))
+#endif
 #endif
