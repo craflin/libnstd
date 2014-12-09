b8ba8f924fe0c77d3cf230970fd38fdc4e9f9320
Colin Graf <colin.graf@sovereign-labs.com>
2014-12-09 23:12:31 +0100

    Time: Fix typos

--- include/nstd/Time.h
+++ include/nstd/Time.h
@@ -7,14 +7,14 @@
 class Time
 {
 public:
-  int_t sec; /**< 0 – 59 */
-  int_t min; /**< 0 – 59 */
-  int_t hour; /**< 0 – 23 */
-  int_t day; /**< 1 – 31 */
+  int_t sec; /**< 0 - 59 */
+  int_t min; /**< 0 - 59 */
+  int_t hour; /**< 0 - 23 */
+  int_t day; /**< 1 - 31 */
   int_t month; /**< 1 - 12 */
   int_t year;
-  int_t wday;  /**< The day of week (0 – 6, 0 = Sunday). */
-  int_t yday; /**< The day of year (0 – 365, 0 = First day of the year). */
+  int_t wday;  /**< The day of week (0 - 6, 0 = Sunday). */
+  int_t yday; /**< The day of year (0 - 365, 0 = First day of the year). */
   bool_t dst;
   bool_t utc;
 
@@ -32,13 +32,15 @@ public:
   bool_t operator==(const Time& other) const;
   bool_t operator!=(const Time& other) const;
 
-  /** Retrieve local system time. The function returns the local system time in milliseconds since 1 January 1970 (Unix time with millisesond precision).
-  * @return The current local system time (in millisconds).
+  /**
+  * Retrieve local system time. The function returns the local system time in milliseconds since 1 January 1970 (Unix time with millisesond precision).
+  * @return The current local system time (in milliseconds).
   */
   static timestamp_t time();
 
-  /** Retrieve ticks (in milliseconds) that have passed since the system was started.
-  * @return The ticks (in millisconds) that have currently passed since the system was started.
+  /**
+  * Retrieve ticks (in milliseconds) that have passed since the system was started.
+  * @return The ticks (in milliseconds) that have currently passed since the system was started.
   */
   static timestamp_t ticks();
 
