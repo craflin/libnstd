04f627b6a10cd90de17a27fc30d4f0085c005607
Colin Graf <colin.graf@sovereign-labs.com>
2015-09-30 21:29:15 +0200

    Separated Unicode test

--- test/UnitTest/Main.cpp
+++ test/UnitTest/Main.cpp
@@ -28,18 +28,7 @@
 void_t testProcess();
 void_t testMonitor();
 void_t testSignal();
-
-void_t testUnicode()
-{
-  if (sizeof(tchar_t) != 1)
-    return;
-  uint32_t ch = 0x5d8;
-  String encoded = Unicode::toString(ch);
-  ASSERT(Unicode::length(*(const tchar_t*)encoded) > 1);
-  ASSERT(Unicode::isValid(encoded));
-  ASSERT(!Unicode::isValid(String()));
-  ASSERT(Unicode::fromString(encoded) == ch);
-}
+void_t testUnicode();
 
 void_t testBuffer()
 {
