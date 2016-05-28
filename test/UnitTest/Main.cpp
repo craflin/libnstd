97f2ca8ff387ed34329517c3cd30b08a32fc80a6
Colin Graf <colin.graf@sovereign-labs.com>
2016-05-28 22:33:01 +0200

    Moved File tests into a separate file

--- test/UnitTest/Main.cpp
+++ test/UnitTest/Main.cpp
@@ -8,7 +8,6 @@
 #include <nstd/HashSet.h>
 #include <nstd/List.h>
 #include <nstd/HashMap.h>
-#include <nstd/File.h>
 #include <nstd/Array.h>
 #include <nstd/Thread.h>
 #include <nstd/Time.h>
@@ -35,6 +34,7 @@ void_t testError();
 void_t testString();
 void_t testMemory();
 void_t testVariant();
+void_t testFile();
 
 void_t testConsolePrintf()
 {
@@ -451,135 +451,6 @@ void_t testNewDelete()
   ASSERT(destructorCalls == 24);
 }
 
-void_t testFile()
-{
-  // test open and close
-  File::unlink(_T("testfile.file.test"));
-  ASSERT(!File::exists(_T("testfile.file.test")));
-  {
-    File file;
-    ASSERT(!file.isOpen());
-    ASSERT(file.open(_T("testfile.file.test"), File::writeFlag));
-    ASSERT(file.isOpen());
-    file.close();
-    ASSERT(!file.isOpen());
-    ASSERT(file.open(_T("testfile.file.test"), File::writeFlag));
-    ASSERT(file.isOpen());
-  }
-
-  // test File::time function
-  {
-    File::Time time;
-    ASSERT(File::time(_T("testfile.file.test"), time));
-    int64_t now = Time::time();
-    //ASSERT(time.accessTime <= now + 1000 && time.accessTime > now - 10000);
-    ASSERT(time.writeTime <= now + 1000 && time.writeTime > now - 10000);
-    //ASSERT(time.creationTime <= now + 1000 && time.creationTime > now - 10000);
-  }
-
-  // test file exists
-  ASSERT(File::exists(_T("testfile.file.test")));
-  ASSERT(!File::exists(_T("dkasdlakshkalal.nonexisting.file")));
-
-  // test file write
-  char_t buffer[266];
-  char_t buffer2[300];
-  {
-    File file;
-    ASSERT(file.open(_T("testfile.file.test"), File::writeFlag));
-    Memory::fill(buffer, 'a', sizeof(buffer));
-    ASSERT(file.write(buffer, sizeof(buffer)) == sizeof(buffer));
-    Memory::fill(buffer2, 'b', sizeof(buffer2));
-    ASSERT(file.write(buffer2, sizeof(buffer2)) == sizeof(buffer2));
-    ASSERT(file.size() == sizeof(buffer) + sizeof(buffer2));
-  }
-
-  // test file read
-  {
-    File file;
-    ASSERT(file.open(_T("testfile.file.test"), File::readFlag));
-    ASSERT(file.size() == sizeof(buffer) + sizeof(buffer2));
-    char_t readBuffer[500];
-    ASSERT(file.read(readBuffer, sizeof(readBuffer)) == sizeof(readBuffer));
-    ASSERT(Memory::compare(readBuffer, buffer, sizeof(buffer)) == 0);
-    ASSERT(Memory::compare(readBuffer + sizeof(buffer), buffer2, sizeof(readBuffer) - sizeof(buffer)) == 0);
-    char_t readBuffer2[166];
-    ASSERT(file.read(readBuffer2, sizeof(readBuffer2)) == sizeof(buffer) + sizeof(buffer2) - sizeof(readBuffer));
-    ASSERT(Memory::compare(readBuffer2, buffer2 + sizeof(buffer2) - (sizeof(buffer) + sizeof(buffer2) - sizeof(readBuffer)), sizeof(buffer) + sizeof(buffer2) - sizeof(readBuffer)) == 0);
-    file.close();
-  }
-
-  // test file read all
-  {
-    File file;
-    ASSERT(file.open(_T("testfile.file.test"), File::readFlag));
-    ASSERT(file.size() == sizeof(buffer) + sizeof(buffer2));
-    String data;
-    ASSERT(file.readAll(data));
-    ASSERT(data.length() * sizeof(tchar_t) == sizeof(buffer) + sizeof(buffer2));
-    ASSERT(Memory::compare((const tchar_t*)data, buffer, sizeof(buffer)) == 0);
-    ASSERT(Memory::compare((const byte_t*)(const tchar_t*)data + sizeof(buffer), buffer2, sizeof(buffer2)) == 0);
-  }
-
-  // test unlink
-  ASSERT(File::unlink(_T("testfile.file.test")));
-  ASSERT(!File::exists(_T("testfile.file.test")));
-
-  // test append mode
-  {
-    char_t buf1[10];
-    char_t buf2[20];
-    Memory::fill(buf1, 1, sizeof(buf1));
-    Memory::fill(buf2, 1, sizeof(buf2));
-    {
-      File file;
-      ASSERT(file.open(_T("testfile.file.test"), File::writeFlag));
-      ASSERT(file.write(buf1, sizeof(buf1)) == sizeof(buf1));
-    }
-    {
-      File file;
-      ASSERT(file.open(_T("testfile.file.test"), File::writeFlag | File::appendFlag));
-      ASSERT(file.write(buf2, sizeof(buf2)) == sizeof(buf2));
-    }
-    {
-      File file;
-      ASSERT(file.open(_T("testfile.file.test"), File::readFlag));
-      char_t result[50];
-      ASSERT(file.read(result, sizeof(result)) == sizeof(buf1) + sizeof(buf2));
-      ASSERT(Memory::compare(result, buf1, sizeof(buf1)) == 0);
-      ASSERT(Memory::compare(result + sizeof(buf1), buf2, sizeof(buf2)) == 0);
-    }
-  }
-  ASSERT(File::unlink(_T("testfile.file.test")));
-}
-
-void_t testFileName()
-{
-  ASSERT(File::basename(_T("c:\\sadasd\\asdas\\test.blah")) == _T("test.blah"));
-  ASSERT(File::basename(_T("c:\\sadasd\\asdas\\test")) == _T("test"));
-  ASSERT(File::basename(_T("c:\\sadasd\\asdas\\test.blah"), _T("blah")) == _T("test"));
-  ASSERT(File::basename(_T("c:\\sadasd\\asdas\\test.blah"), _T(".blah")) == _T("test"));
-  ASSERT(File::extension(_T("c:\\sadasd\\asdas\\test.blah")) == _T("blah"));
-  ASSERT(File::dirname(_T("c:\\sadasd\\asdas\\test.blah")) == _T("c:\\sadasd\\asdas"));
-  ASSERT(File::dirname(_T("asdas/test.blah")) == _T("asdas"));
-
-  ASSERT(File::simplifyPath(_T("../../dsadsad/2dsads")) == _T("../../dsadsad/2dsads"));
-  ASSERT(File::simplifyPath(_T("..\\..\\dsadsad\\2dsads")) == _T("../../dsadsad/2dsads"));
-  ASSERT(File::simplifyPath(_T(".././../dsadsad/2dsads")) == _T("../../dsadsad/2dsads"));
-  ASSERT(File::simplifyPath(_T("dsadsad/../2dsads")) == _T("2dsads"));
-  ASSERT(File::simplifyPath(_T("dsadsad/./../2dsads")) == _T("2dsads"));
-  ASSERT(File::simplifyPath(_T("dsadsad/.././2dsads")) == _T("2dsads"));
-  ASSERT(File::simplifyPath(_T("/dsadsad/../2dsads")) == _T("/2dsads"));
-  ASSERT(File::simplifyPath(_T("/../../aaa/2dsads")) == _T("/../../aaa/2dsads"));
-
-  ASSERT(File::isAbsolutePath(_T("/aaa/2dsads")));
-  ASSERT(File::isAbsolutePath(_T("\\aaa\\2dsads")));
-  ASSERT(File::isAbsolutePath(_T("c:/aaa/2dsads")));
-  ASSERT(File::isAbsolutePath(_T("c:\\aaa\\2dsads")));
-  ASSERT(!File::isAbsolutePath(_T("..\\aaa\\2dsads")));
-  ASSERT(!File::isAbsolutePath(_T("aaa/2dsads")));
-}
-
 void_t testArray()
 {
   // test append
@@ -872,7 +743,6 @@ int_t main(int_t argc, char_t* argv[])
   testHashMapString();
   testNewDelete();
   testFile();
-  testFileName();
   testArray();
   testArrayString();
   testDirectory();
