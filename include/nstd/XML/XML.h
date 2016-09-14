0a1dae13ab05cf3c780fae7e7e779041f5d58ff9
Colin Graf <colin.graf@sovereign-labs.com>
2016-09-14 20:53:36 +0200

    Added initial XML parser implementation

---
+++ include/nstd/XML/XML.h
@@ -0,0 +1,199 @@
+
+#pragma once
+
+#include <nstd/String.h>
+#include <nstd/HashMap.h>
+#include <nstd/List.h>
+
+class XML
+{
+public:
+  class Element;
+
+  class Variant
+  {
+  public:
+    enum Type
+    {
+      nullType,
+      elementType,
+      textType,
+    };
+
+  public:
+
+    Variant() : data(&nullData) {}
+    ~Variant() {clear();}
+
+    Variant(const Variant& other)
+    {
+      if(other.data->ref)
+      {
+        data = other.data;
+        Atomic::increment(data->ref);
+      }
+      else
+        data = &nullData;
+    }
+
+    Variant(const Element& val)
+    {
+      data = (Data*)Memory::alloc(sizeof(Data) + sizeof(Element));
+      Element* element = (Element*)(data + 1);
+      new (element) Element(val);
+      data->type = elementType;
+      data->ref = 1;
+    }
+
+    Variant(const String& val)
+    {
+      data = (Data*)Memory::alloc(sizeof(Data) + sizeof(String));
+      String* string = (String*)(data + 1);
+      new (string) String(val);
+      data->type = textType;
+      data->ref = 1;
+    }
+
+    void_t clear()
+    {
+      if(data->ref && Atomic::decrement(data->ref) == 0)
+      {
+        switch(data->type)
+        {
+        case elementType: ((Element*)(data + 1))->~Element(); break;
+        case textType: ((String*)(data + 1))->~String(); break;
+        default: break;
+        }
+        Memory::free(data);
+      }
+      data = &nullData;
+    }
+
+    Type getType() const {return data->type;}
+    bool_t isNull() const {return data->type == nullType;}
+
+    bool_t isText() const {return data->type == textType;}
+
+    String toString() const
+    {
+      if(data->type == textType)
+        return *(const String*)(data + 1);
+      return String();
+    }
+
+    Variant& operator=(const String& other)
+    {
+      if(data->type != textType || data->ref > 1)
+      {
+        clear();
+        data = (Data*)Memory::alloc(sizeof(Data) + sizeof(String));
+        String* string = (String*)(data + 1);
+        new (string) String(other);
+        data->type = textType;
+        data->ref = 1;
+      }
+      else
+        *(String*)(data + 1) = other;
+      return *this;
+    }
+
+    bool_t isElement() const {return data->type == elementType;}
+
+    const Element& toElement() const
+    {
+      if(data->type == elementType)
+        return *(const Element*)(data + 1);
+      static const Element element;
+      return element;
+    }
+
+    Element& toElement()
+    {
+      if(data->type != elementType)
+      {
+        clear();
+        data = (Data*)Memory::alloc(sizeof(Data) + sizeof(List<Variant>));
+        Element* element = (Element*)(data + 1);
+        new (element) Element;
+        data->type = elementType;
+        data->ref = 1;
+        return *element;
+      }
+      else if(data->ref > 1)
+      {
+        Data* newData = (Data*)Memory::alloc(sizeof(Data) + sizeof(List< Variant>));
+        Element* element = (Element*)(data + 1);
+        new (element) Element(*(const Element*)(data + 1));
+        clear();
+        data = newData;
+        data->type = elementType;
+        data->ref = 1;
+        return *element;
+      }
+      return *(Element*)(data + 1);
+    }
+
+  private:
+    struct Data
+    {
+      Type type;
+      volatile size_t ref;
+    };
+
+    static struct NullData : public Data
+    {
+      NullData()
+      {
+        type = nullType;
+        ref = 0;
+      }
+    } nullData;
+
+    Data* data;
+    Data _data;
+  };
+
+  class Element
+  {
+  public:
+    String type;
+    HashMap<String, String> attributes;
+    List<Variant> content;
+
+  public:
+    void_t clear()
+    {
+      type.clear();
+      attributes.clear();
+      content.clear();
+    }
+
+    String toString() const;
+
+  private:
+    class Private;
+  };
+
+  class Parser
+  {
+  public:
+    Parser();
+    ~Parser();
+
+    int_t getErrorLine() const;
+    int_t getErrorColumn() const;
+    String getErrorString() const;
+
+    bool_t parse(const String& data, Element& element);
+
+  private:
+    class Private;
+    Private* p;
+  };
+
+public:
+  static bool_t parse(const String& data, Element& element);
+  static bool_t load(const String& file, Element& element);
+  static bool_t save(const Element& element, const String& file);
+  static String toString(const Element& element);
+};
---
+++ src/XML/XML.cpp
@@ -0,0 +1,434 @@
+
+#include <nstd/Error.h>
+#include <nstd/File.h>
+#include <nstd/XML/XML.h>
+
+class XML::Parser::Private
+{
+public:
+  class Position
+  {
+  public:
+    int_t line;
+    const char_t* pos;
+  };
+
+  class Token
+  {
+  public:
+    enum Type
+    {
+      startTagBeginType, // <
+      tagEndType, // >
+      endTagBeginType, // </
+      emptyTagEndType, // />
+      equalsSignType, // =
+      stringType,
+      nameType, // attribute or tag name
+      eofType,
+    };
+
+  public:
+    Type type;
+    String value;
+    Position pos;
+  };
+
+public:
+  Token token;
+  const char_t* start;
+  Position pos;
+
+  int_t errorLine;
+  int_t errorColumn;
+  String errorString;
+
+  bool_t parse(const String& data, Element& element);
+  bool_t parseElement(Element& element);
+  bool_t parseText(String& text);
+
+  bool_t readToken();
+
+  void_t skipSpace();
+
+  void_t syntaxError(const Position& pos, const String& error);
+
+  static String unescapeString(const String& str);
+};
+
+class XML::Element::Private
+{
+public:
+  static String escapeString(const String& str);
+};
+
+bool_t XML::Parser::Private::readToken()
+{
+  skipSpace();
+  switch(*pos.pos)
+  {
+  case '<':
+    if(pos.pos[1] == '/')
+    {
+      token.pos = pos;
+      token.type = Token::endTagBeginType;
+      pos.pos += 2;
+      return true;
+    }
+    token.pos = pos;
+    token.type = Token::startTagBeginType;
+    ++pos.pos;
+    return true;
+  case '>':
+    token.pos = pos;
+    token.type = Token::tagEndType;
+    ++pos.pos;
+    return true;
+  case '\0':
+    token.pos = pos;
+    token.type = Token::eofType;
+    return true;
+  case '=':
+    token.pos = pos;
+    token.type = Token::equalsSignType;
+    ++pos.pos;
+    return true;
+  case '"':
+  case '\'':
+    {
+      char_t endChars[4] = "x\r\n";
+      *endChars = *pos.pos;
+      const char_t* end = String::findOneOf(pos.pos + 1, endChars);
+      if(!end)
+        return syntaxError(pos, "Unexpected end of file"), false;
+      if(*end != *pos.pos)
+        return syntaxError(pos, "New line in string"), false;
+      token.pos = pos;
+      token.value = unescapeString(String(pos.pos + 1, end - pos.pos - 2));
+      token.type = Token::stringType;
+      pos.pos = end + 1;
+      return true;
+    }
+  case '/':
+    if(pos.pos[1] == '>')
+    {
+      token.pos = pos;
+      token.type = Token::emptyTagEndType;
+      pos.pos += 2;
+    return true;
+    }
+    // no break
+  default: // attribute or tag name
+    {
+      const char_t* end = pos.pos;
+      while(*end && *end != '/' && *end != '>' && *end != '=' && !String::isSpace(*end))
+        ++end;
+      if(end == pos.pos)
+        return syntaxError(pos, "Expected name"), false;
+      token.pos = pos;
+      token.value = String(pos.pos, end - pos.pos);
+      token.type = Token::nameType;
+      pos.pos = end;
+      return true;
+    }
+  }
+}
+
+void_t XML::Parser::Private::skipSpace()
+{
+  for(char_t c;;)
+    switch((c = *pos.pos))
+    {
+    case '\r':
+      if(*(++pos.pos) == '\n')
+        ++pos.pos;
+      ++pos.line;
+      continue;
+    case '\n':
+      ++pos.line;
+      ++pos.pos;
+      continue;
+    case '<':
+      if(String::compare(pos.pos + 1, "!--") == 0)
+      {
+        const char_t* end = pos.pos + 4;
+        for(;;)
+        {
+          int_t line = pos.line;
+          const char_t* newEnd = String::findOneOf(end, "-\n\r");
+          if(!newEnd)
+          {
+            pos.pos = end + String::length(end);
+            pos.line = line;
+            return;
+          }
+          end = newEnd;
+          switch(*newEnd)
+          {
+          case '\r':
+            if(*(++end) == '\n')
+              ++end;
+            ++line;
+            continue;
+          case '\n':
+            ++line;
+            ++end;
+            continue;
+          default:
+            if(String::compare(end + 1, "->") == 0)
+            {
+              pos.pos = end + 3;
+              pos.line = line;
+              break;
+            }
+            ++end;
+            continue;
+          }
+          break;
+        }
+        continue;
+      }
+    default:
+      if(String::isSpace(c))
+        ++pos.pos;
+      else
+        return;
+    }
+}
+
+void_t XML::Parser::Private::syntaxError(const Position& pos, const String& error)
+{
+  int column = 1;
+  for(const char_t* p = pos.pos; p > start;)
+  {
+    --p;
+    if(*p == '\n' || *p == '\r')
+      break;
+    ++column;
+  }
+  errorLine = pos.line;
+  errorColumn = column;
+  errorString = error;
+}
+
+String XML::Parser::Private::unescapeString(const String& str)
+{
+  // todo
+  return str;
+}
+
+String XML::Element::Private::escapeString(const String& str)
+{
+  // todo
+  return str;
+}
+
+bool_t XML::Parser::Private::parse(const String& data, Element& element)
+{
+  start = data;
+  pos.line = 1;
+  pos.pos = start;
+
+  skipSpace();
+  if(*pos.pos == '<' && pos.pos[1] == '?')
+  {
+    Position pos = this->pos;
+    for(;;)
+    {
+      const char* end = String::findOneOf(pos.pos, "\r\n?");
+      if(!end)
+        return this->syntaxError(pos, "Unexpected end of file"), false;
+      if(*end == '?' && end[1] == '>')
+      {
+        pos.pos = end + 2;
+        break;
+      }
+      pos.pos = end;
+      skipSpace();
+    }
+  }
+  if(!readToken())
+    return false;
+  if(token.type != Token::startTagBeginType)
+    return syntaxError(token.pos, "Expected '<'"), false;
+  return parseElement(element);
+}
+
+bool_t XML::Parser::Private::parseElement(Element& element)
+{
+  if(!readToken())
+    return false;
+  if(token.type != Token::nameType)
+    return syntaxError(token.pos, "Expected tag name"), false;
+  element.type = token.value;
+  for(;;)
+  {
+    if(!readToken())
+      return false;
+    if(token.type == Token::emptyTagEndType)
+      return true;
+    if(token.type == Token::tagEndType)
+      break;
+    if(token.type == Token::nameType)
+    {
+      String attributeName = token.value;
+      if(!readToken())
+        return false;
+      if(token.type != Token::equalsSignType)
+        return syntaxError(token.pos, "Expected '='"), false;
+      if(!readToken())
+        return false;
+      if(token.type != Token::stringType)
+        return syntaxError(token.pos, "Expected string"), false;
+      element.attributes.append(attributeName, token.value);
+      continue;
+    }
+  }
+  for(;;)
+  {
+    Position pos = this->pos;
+    if(readToken())
+    {
+      if(token.type == Token::endTagBeginType)
+        break;
+      if(token.type == Token::startTagBeginType)
+      {
+        Variant& variant = element.content.append(Variant());
+        if(!parseElement(variant.toElement()))
+          return false;
+      }
+      else
+        this->pos = pos;
+    }
+    Variant& variant = element.content.append(Variant());
+    if(!parseText(variant.toString()))
+      return false;
+  }
+  if(!readToken())
+    return false;
+  if(token.type != Token::nameType)
+    return syntaxError(token.pos, "Expected tag name"), false;
+  if(token.value != element.type)
+    return syntaxError(token.pos, String("Expected end tag of '") + element.type + "'"), false;
+  if(!readToken())
+    return false;
+  if(token.type != Token::tagEndType)
+    return syntaxError(token.pos, "Expected '>'"), false;
+  return true;
+}
+
+bool_t XML::Parser::Private::parseText(String& text)
+{
+  const char_t* start = pos.pos;
+  int_t line = pos.line;
+  const char_t* end = pos.pos;
+  for(;;)
+  {
+    const char_t* newEnd = String::findOneOf(end, "<\r\n");
+    if(!newEnd)
+    {
+      pos.pos = end + String::length(end);
+      return syntaxError(pos, "Unexpected end of file"), false;
+    }
+    end = newEnd;
+    switch(*end)
+    {
+    case '\r':
+      if(*(++end) == '\n')
+        ++end;
+      ++line;
+      continue;
+    case '\n':
+      ++line;
+      ++end;
+      continue;
+    default:
+      pos.pos = end;
+      pos.line = line;
+      text = String(start, end - start);
+      return true;
+    }
+  }
+}
+
+XML::Parser::Parser() : p(new Private) {}
+XML::Parser::~Parser() {delete p;}
+
+int_t XML::Parser::getErrorLine() const {return p->errorLine;}
+int_t XML::Parser::getErrorColumn() const {return p->errorColumn;}
+String XML::Parser::getErrorString() const {return p->errorString;}
+
+bool_t XML::Parser::parse(const String& data, Element& element) {return p->parse(data, element);}
+
+bool_t XML::parse(const String& data, Element& element)
+{
+  Parser parser;
+  if(parser.parse(data, element))
+    return true;
+  Error::setErrorString(String::fromPrintf("Syntax error at line %d, column %d: %s", parser.getErrorLine(), parser.getErrorColumn(), (const char_t*)parser.getErrorString()));
+  return false;
+}
+
+bool_t XML::load(const String& filePath, Element& element)
+{
+  File file;
+  if(!file.open(filePath))
+    return false;
+  String data;
+  if(!file.readAll(data))
+    return false;
+  return parse(data, element);
+}
+
+bool_t XML::save(const Element& element, const String& filePath)
+{
+  File file;
+  if(!file.open(filePath, File::writeFlag))
+    return false;
+  return file.write(toString(element));
+}
+
+String XML::toString(const Element& element)
+{
+  String result("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+  result += element.toString();
+  return result;
+}
+
+String XML::Element::toString() const
+{
+  String result(type.length() + 200);
+  result.append('<');
+  result.append(type);
+  for(HashMap<String, String>::Iterator i = attributes.begin(), end = attributes.end(); i != end; ++i)
+  {
+    result.append(' ');
+    result.append(i.key());
+    result.append("=\"");
+    result.append(Private::escapeString(*i));
+    result.append('"');
+  }
+  if(content.isEmpty())
+    result.append("/>");
+  else
+  {
+    for(List<Variant>::Iterator i = content.begin(), end = content.end(); i != end; ++i)
+    {
+      const Variant& variant = *i;
+      switch(variant.getType())
+      {
+      case Variant::elementType:
+        result.append(variant.toElement().toString());
+        break;
+      case Variant::textType:
+        result.append(variant.toString());
+        break;
+      default: ;
+      }
+    }
+    result.append("</");
+    result.append(type);
+    result.append('>');
+  }
+  return result;
+}
