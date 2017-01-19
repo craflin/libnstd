5a6e12d9418b0916e1855860860e0c61981db9cc
Colin Graf <colin.graf@sovereign-labs.com>
2017-01-19 21:14:19 +0100

    Atomic: Moved tests into a separate file

--- test/UnitTest/Main.cpp
+++ test/UnitTest/Main.cpp
@@ -3,7 +3,6 @@
 #include <nstd/Memory.h>
 #include <nstd/Debug.h>
 #include <nstd/Mutex.h>
-#include <nstd/Atomic.h>
 #include <nstd/String.h>
 #include <nstd/HashSet.h>
 #include <nstd/List.h>
@@ -18,6 +17,7 @@
 
 #include <cstdlib>
 
+void testAtomic();
 void testProcess();
 void testMonitor();
 void testSignal();
@@ -60,55 +60,6 @@ void testDebugPrintf()
   buffer[bufferSize - 2] = 'b';
   buffer[bufferSize - 1] = '\0';
   Debug::printf(_T("%hs%hs\n"), buffer, buffer);
-}
-
-void testAtomic()
-{
-  volatile usize size = 0;
-  ASSERT(Atomic::increment(size) == 1);
-  ASSERT(Atomic::increment(size) == 2);
-  ASSERT(Atomic::increment(size) == 3);
-  ASSERT(Atomic::decrement(size) == 2);
-  ASSERT(Atomic::decrement(size) == 1);
-  ASSERT(Atomic::decrement(size) == 0);
-  volatile int32 int32 = 0;
-  ASSERT(Atomic::increment(int32) == 1);
-  ASSERT(Atomic::decrement(int32) == 0);
-  ASSERT(Atomic::decrement(int32) == -1);
-  volatile uint32 uint32 = 0xfffffff0;
-  ASSERT(Atomic::increment(uint32) == 0xfffffff1);
-  ASSERT(Atomic::decrement(uint32) == 0xfffffff0);
-#ifndef _ARM
-  volatile int64 int64 = 0;
-  ASSERT(Atomic::increment(int64) == 1);
-  ASSERT(Atomic::decrement(int64) == 0);
-  ASSERT(Atomic::decrement(int64) == -1);
-  volatile uint64 uint64 =  (0xffffffffULL << 32) | 0xfffffff0ULL;
-  ASSERT(Atomic::increment(uint64) == ((0xffffffffULL << 32) | 0xfffffff1ULL));
-  ASSERT(Atomic::decrement(uint64) == ((0xffffffffULL << 32) | 0xfffffff0ULL));
-#endif
-
-  void* volatile ptr = 0;
-  ASSERT(Atomic::compareAndSwap(ptr, 0, (void*)0xff) == 0);
-  ASSERT(Atomic::compareAndSwap(ptr, (void*)0xff, 0) == (void*)0xff);
-  ASSERT(Atomic::compareAndSwap(ptr, (void*)23, 0) == 0);
-  
-  size = 0;
-  ASSERT(Atomic::swap(size, 1) == 0);
-  ASSERT(Atomic::swap(size, 2) == 1);
-  int32 = 0;
-  ASSERT(Atomic::swap(int32, 1) == 0);
-  ASSERT(Atomic::swap(int32, -1) == 1);
-  ASSERT(Atomic::swap(int32, 2) == -1);
-  uint32 = 0;
-  ASSERT(Atomic::swap(uint32, 1) == 0);
-  ASSERT(Atomic::swap(uint32, 2) == 1);
-  uint64 = 0;
-  ASSERT(Atomic::swap(uint64, 1) == 0);
-  ASSERT(Atomic::swap(uint64, 2) == 1);
-  ptr = 0;
-  ASSERT(Atomic::swap(ptr, (void*)0xffffffff) == 0);
-  ASSERT(Atomic::swap(ptr, (void*)123) == (void*)0xffffffff);
 }
 
 void testHashSet()
---
+++ test/UnitTest/TestAtomic.cpp
@@ -0,0 +1,52 @@
+
+#include <nstd/Debug.h>
+#include <nstd/Atomic.h>
+
+void testAtomic()
+{
+  volatile usize size = 0;
+  ASSERT(Atomic::increment(size) == 1);
+  ASSERT(Atomic::increment(size) == 2);
+  ASSERT(Atomic::increment(size) == 3);
+  ASSERT(Atomic::decrement(size) == 2);
+  ASSERT(Atomic::decrement(size) == 1);
+  ASSERT(Atomic::decrement(size) == 0);
+  volatile int32 int32 = 0;
+  ASSERT(Atomic::increment(int32) == 1);
+  ASSERT(Atomic::decrement(int32) == 0);
+  ASSERT(Atomic::decrement(int32) == -1);
+  volatile uint32 uint32 = 0xfffffff0;
+  ASSERT(Atomic::increment(uint32) == 0xfffffff1);
+  ASSERT(Atomic::decrement(uint32) == 0xfffffff0);
+#ifndef _ARM
+  volatile int64 int64 = 0;
+  ASSERT(Atomic::increment(int64) == 1);
+  ASSERT(Atomic::decrement(int64) == 0);
+  ASSERT(Atomic::decrement(int64) == -1);
+  volatile uint64 uint64 =  (0xffffffffULL << 32) | 0xfffffff0ULL;
+  ASSERT(Atomic::increment(uint64) == ((0xffffffffULL << 32) | 0xfffffff1ULL));
+  ASSERT(Atomic::decrement(uint64) == ((0xffffffffULL << 32) | 0xfffffff0ULL));
+#endif
+
+  void* volatile ptr = 0;
+  ASSERT(Atomic::compareAndSwap(ptr, 0, (void*)0xff) == 0);
+  ASSERT(Atomic::compareAndSwap(ptr, (void*)0xff, 0) == (void*)0xff);
+  ASSERT(Atomic::compareAndSwap(ptr, (void*)23, 0) == 0);
+  
+  size = 0;
+  ASSERT(Atomic::swap(size, 1) == 0);
+  ASSERT(Atomic::swap(size, 2) == 1);
+  int32 = 0;
+  ASSERT(Atomic::swap(int32, 1) == 0);
+  ASSERT(Atomic::swap(int32, -1) == 1);
+  ASSERT(Atomic::swap(int32, 2) == -1);
+  uint32 = 0;
+  ASSERT(Atomic::swap(uint32, 1) == 0);
+  ASSERT(Atomic::swap(uint32, 2) == 1);
+  uint64 = 0;
+  ASSERT(Atomic::swap(uint64, 1) == 0);
+  ASSERT(Atomic::swap(uint64, 2) == 1);
+  ptr = 0;
+  ASSERT(Atomic::swap(ptr, (void*)0xffffffff) == 0);
+  ASSERT(Atomic::swap(ptr, (void*)123) == (void*)0xffffffff);
+}
