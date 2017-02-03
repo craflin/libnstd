922797d47fb86b0b7907e8ea5c12a25fd04cbc42
Colin Graf <colin.graf@sovereign-labs.com>
2017-02-03 22:44:26 +0100

    Time: Moved Time tests into a separate file

---
+++ test/UnitTest/TestTime.cpp
@@ -0,0 +1,39 @@
+
+#include <nstd/String.h>
+#include <nstd/Debug.h>
+#include <nstd/Time.h>
+
+void testTime()
+{
+  String test = Time::toString(123 * 1000, _T("%Y-%m-%d %H:%M:%S"), true);
+  ASSERT(test == _T("1970-01-01 00:02:03"));
+
+  {
+    Time time(123LL * 1000, true);
+    ASSERT(time.toString(_T("%Y-%m-%d %H:%M:%S")) == test);
+    ASSERT(time.year == 1970);
+    ASSERT(time.month == 1);
+    ASSERT(time.day == 1);
+    ASSERT(time.hour == 0);
+    ASSERT(time.min == 2);
+    ASSERT(time.sec == 3);
+  }
+
+  int64 now = Time::time();
+  Time time(now);
+  ASSERT(time.toTimestamp() == now);
+  Time time2(time);
+  time2.toLocal();
+  ASSERT(time == time2);
+  ASSERT(time2.toTimestamp() == now);
+
+  Time timeUtc(time);
+  timeUtc.toUtc();
+  ASSERT(timeUtc != time);
+  ASSERT(timeUtc.toTimestamp() == now);
+  Time timeUtc2(timeUtc);
+  timeUtc2.toUtc();
+  ASSERT(timeUtc != time);
+  ASSERT(timeUtc2 == timeUtc);
+  ASSERT(timeUtc2.toTimestamp() == now);
+}
