8d8d4aeab6694ace0c6e59cd6f499b6bd44758d9
Colin Graf <colin.graf@sovereign-labs.com>
2017-04-02 11:25:34 +0200

    RefCount: Fixed self assigning issue

--- include/nstd/RefCount.h
+++ include/nstd/RefCount.h
@@ -44,46 +44,40 @@ public:
     {
       Object* refObj = obj;
       if(refObj && Atomic::decrement(refObj->ref) == 0)
-        delete obj;
+        delete refObj;
     }
 
     Ptr& operator=(const Ptr& other)
     {
-      Object* refObj = obj;
-      if(refObj && Atomic::decrement(refObj->ref) == 0)
-        delete obj;
-     if((obj = other.obj))
-     {
-        Object* refObj = obj;
-        Atomic::increment(refObj->ref);
-     }
-     return *this;
+      Object* refObj;
+      if(other.obj)
+        refObj = other.obj, Atomic::increment(refObj->ref);
+      if((refObj = obj) && Atomic::decrement(refObj->ref) == 0)
+        delete refObj;
+      obj = other.obj;
+      return *this;
     }
 
     Ptr& operator=(C* obj)
     {
-      Object* refObj = this->obj;
-      if(refObj && Atomic::decrement(refObj->ref) == 0)
-        delete this->obj;
-     if((this->obj = obj))
-     {
-        refObj = obj;
-        Atomic::increment(refObj->ref);
-     }
+      Object* refObj;
+      if(obj)
+        refObj = obj, Atomic::increment(refObj->ref);
+      if((refObj = this->obj) && Atomic::decrement(refObj->ref) == 0)
+        delete refObj;
+      this->obj = obj;
      return *this;
     }
 
     template <class D> Ptr& operator=(const Ptr<D>& other)
     {
-      Object* refObj = obj;
-      if(refObj && Atomic::decrement(refObj->ref) == 0)
-        delete obj;
-     if((obj = other.obj))
-     {
-        Object* refObj = obj;
-        Atomic::increment(refObj->ref);
-     }
-     return *this;
+      Object* refObj;
+      if(other.obj)
+        refObj = other.obj, Atomic::increment(refObj->ref);
+      if((refObj = obj) && Atomic::decrement(refObj->ref) == 0)
+        delete refObj;
+      obj = other.obj;
+      return *this;
     }
 
     C& operator*() const {return *obj;}
@@ -113,10 +107,15 @@ public:
   {
   public:
     Object() : ref(0) {}
+
+  protected:
     virtual ~Object() {}
 
   private:
     usize ref;
+
+    Object(const Object&);
+    Object& operator=(const Object&);
 
     template<class C> friend class Ptr;
   };
