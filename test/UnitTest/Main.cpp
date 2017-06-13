4d9f2e15bc644163091e702677391790e3d62ca6
Colin Graf <colin.graf@sovereign-labs.com>
2017-06-13 22:18:06 +0200

    Separated Debug test

--- test/UnitTest/Main.cpp
+++ test/UnitTest/Main.cpp
@@ -16,6 +16,7 @@
 
 void testAtomic();
 void testConsole();
+void testDebug();
 void testProcess();
 void testMonitor();
 void testSignal();
@@ -43,17 +44,6 @@ void testMap();
 void testSha256();
 void testMap();
 void testRefCount();
-
-void testDebugPrintf()
-{
-  Debug::printf(_T("%s\n"), _T("Hello world"));
-  usize bufferSize;
-  char* buffer = (char*)Memory::alloc(5000 * 4, bufferSize);
-  Memory::fill(buffer, 'a', bufferSize - 1);
-  buffer[bufferSize - 2] = 'b';
-  buffer[bufferSize - 1] = '\0';
-  Debug::printf(_T("%hs%hs\n"), buffer, buffer);
-}
 
 void testHashSet()
 {
@@ -553,7 +543,7 @@ int main(int argc, char* argv[])
   testSempahore();
   testMutex();
   //testConsole();
-  //testDebugPrintf();
+  //testDebug();
   testAtomic();
   testString();
   testHashSet();
---
+++ test/UnitTest/TestDebug.cpp
@@ -0,0 +1,14 @@
+
+#include <nstd/Debug.h>
+#include <nstd/Memory.h>
+
+void testDebug()
+{
+  Debug::printf(_T("%s\n"), _T("Hello world"));
+  usize bufferSize;
+  char* buffer = (char*)Memory::alloc(5000 * 4, bufferSize);
+  Memory::fill(buffer, 'a', bufferSize - 1);
+  buffer[bufferSize - 2] = 'b';
+  buffer[bufferSize - 1] = '\0';
+  Debug::printf(_T("%hs%hs\n"), buffer, buffer);
+}
