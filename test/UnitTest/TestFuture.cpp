833b8f63ea8a29985f030d41df846e0ba5bd19b1
Colin Graf <colin.graf@sovereign-labs.com>
2017-06-25 16:01:42 +0200

    Future: Added more tests

--- test/UnitTest/TestFuture.cpp
+++ test/UnitTest/TestFuture.cpp
@@ -11,37 +11,7 @@ void testFuture()
     bool called;
 
   public:
-    FutureTest() : called(false), check(42) {}
-
-    static usize staticTest0()
-    {
-      return 32;
-    }
-
-    static usize staticTest1(const String& str)
-    {
-      return str.length();
-    }
-
-    static usize staticTest5(const String& str1, const String& str2, const String& str3, const String& str4, const String& str5)
-    {
-      return str1.length() + str2.length() + str3.length() + str4.length() + str5.length();
-    }
-
-    usize memberTest0()
-    {
-      return check;
-    }
-
-    usize memberTest1(const String& str)
-    {
-      return check + str.length();
-    }
-
-    usize memberTest4(const String& str1, const String& str2, const String& str3, const String& str4)
-    {
-      return check + str1.length() + str2.length() + str3.length() + str4.length();
-    }
+    FutureTest() : check(42) {}
 
     static void staticVoidTest0()
     {
@@ -50,6 +20,27 @@ void testFuture()
     static void staticVoidTest1(int check)
     {
       ASSERT(check == 32);
+    }
+
+    static void staticVoidTest2(int check, const String& str1)
+    {
+      ASSERT(check == 32);
+      ASSERT(str1 == "hallo1");
+    }
+
+    static void staticVoidTest3(int check, const String& str1, const String& str2)
+    {
+      ASSERT(check == 32);
+      ASSERT(str1 == "hallo1");
+      ASSERT(str2 == "hallo2");
+    }
+
+    static void staticVoidTest4(int check, const String& str1, const String& str2, const String& str3)
+    {
+      ASSERT(check == 32);
+      ASSERT(str1 == "hallo1");
+      ASSERT(str2 == "hallo2");
+      ASSERT(str3 == "hallo3");
     }
 
     static void staticVoidTest5(int check, const String& str1, const String& str2, const String& str3, const String& str4)
@@ -72,6 +63,21 @@ void testFuture()
       ASSERT(this->check + check == 42 + 32);
     }
 
+    void memberVoidTest2(int check, const String& str1)
+    {
+      called = true;
+      ASSERT(this->check + check == 42 + 32);
+      ASSERT(str1 == "hallo1");
+    }
+
+    void memberVoidTest3(int check, const String& str1, const String& str2)
+    {
+      called = true;
+      ASSERT(this->check + check == 42 + 32);
+      ASSERT(str1 == "hallo1");
+      ASSERT(str2 == "hallo2");
+    }
+
     void memberVoidTest4(int check, const String& str1, const String& str2, const String& str3)
     {
       called = true;
@@ -79,6 +85,61 @@ void testFuture()
       ASSERT(str1 == "hallo1");
       ASSERT(str2 == "hallo2");
       ASSERT(str3 == "hallo3");
+    }
+
+    static usize staticTest0()
+    {
+      return 32;
+    }
+
+    static usize staticTest1(const String& str)
+    {
+      return str.length();
+    }
+
+    static usize staticTest2(const String& str1, const String& str2)
+    {
+      return str1.length() + str2.length();
+    }
+
+    static usize staticTest3(const String& str1, const String& str2, const String& str3)
+    {
+      return str1.length() + str2.length() + str3.length();
+    }
+
+    static usize staticTest4(const String& str1, const String& str2, const String& str3, const String& str4)
+    {
+      return str1.length() + str2.length() + str3.length() + str4.length();
+    }
+
+    static usize staticTest5(const String& str1, const String& str2, const String& str3, const String& str4, const String& str5)
+    {
+      return str1.length() + str2.length() + str3.length() + str4.length() + str5.length();
+    }
+
+    usize memberTest0()
+    {
+      return check;
+    }
+
+    usize memberTest1(const String& str)
+    {
+      return check + str.length();
+    }
+
+    usize memberTest2(const String& str1, const String& str2)
+    {
+      return check + str1.length() + str2.length();
+    }
+
+    usize memberTest3(const String& str1, const String& str2, const String& str3)
+    {
+      return check + str1.length() + str2.length() + str3.length();
+    }
+
+    usize memberTest4(const String& str1, const String& str2, const String& str3, const String& str4)
+    {
+      return check + str1.length() + str2.length() + str3.length() + str4.length();
     }
 
   private:
@@ -99,134 +160,145 @@ void testFuture()
 
   {
     Future<void> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
     future.start(&FutureTest::staticVoidTest1, 32);
     future.join();
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
   }
 
   {
     Future<void> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
+    future.start(&FutureTest::staticVoidTest2, 32, String("hallo1"));
+    future.join();
+  }
+
+  {
+    Future<void> future;
+    future.start(&FutureTest::staticVoidTest3, 32, String("hallo1"), String("hallo2"));
+    future.join();
+  }
+
+  {
+    Future<void> future;
+    future.start(&FutureTest::staticVoidTest4, 32, String("hallo1"), String("hallo2"), String("hallo3"));
+    future.join();
+  }
+
+  {
+    Future<void> future;
     future.start(&FutureTest::staticVoidTest5, 32, String("hallo1"), String("hallo2"), String("hallo3"), String("hallo4"));
     future.join();
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
   }
 
   {
     Future<void> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
     FutureTest t;
     future.start(t, &FutureTest::memberVoidTest0);
     future.join();
     ASSERT(t.called);
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
   }
 
   {
     Future<void> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
     FutureTest t;
     future.start(t, &FutureTest::memberVoidTest1, 32);
     future.join();
     ASSERT(t.called);
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
   }
 
   {
     Future<void> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
+    FutureTest t;
+    future.start(t, &FutureTest::memberVoidTest2, 32, String("hallo1"));
+    future.join();
+    ASSERT(t.called);
+  }
+
+  {
+    Future<void> future;
+    FutureTest t;
+    future.start(t, &FutureTest::memberVoidTest3, 32, String("hallo1"), String("hallo2"));
+    future.join();
+    ASSERT(t.called);
+  }
+
+  {
+    Future<void> future;
     FutureTest t;
     future.start(t, &FutureTest::memberVoidTest4, 32, String("hallo1"), String("hallo2"), String("hallo3"));
     future.join();
     ASSERT(t.called);
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
   }
 
   {
     Future<usize> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
     future.start(&FutureTest::staticTest0);
     future.join();
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
     ASSERT(future == 32);
   }
 
   {
     Future<usize> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
     future.start(&FutureTest::staticTest1, String("hello"));
     future.join();
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
     ASSERT(future == 5);
   }
 
   {
     Future<usize> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
+    future.start(&FutureTest::staticTest2, String("hello"), String("hello"));
+    future.join();
+    ASSERT(future == 10);
+  }
+
+  {
+    Future<usize> future;
+    future.start(&FutureTest::staticTest3, String("hello"), String("hello"), String("hello"));
+    future.join();
+    ASSERT(future == 15);
+  }
+
+  {
+    Future<usize> future;
+    future.start(&FutureTest::staticTest4, String("hello"), String("hello"), String("hello"), String("hello"));
+    future.join();
+    ASSERT(future == 20);
+  }
+
+  {
+    Future<usize> future;
     future.start(&FutureTest::staticTest5, String("hello"), String("hello"), String("hello"), String("hello"), String("hello"));
     future.join();
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
     ASSERT(future == 25);
   }
 
   {
     Future<usize> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
     FutureTest t;
     future.start(t, &FutureTest::memberTest1, String("hello"));
     future.join();
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
     ASSERT(future == 42 + 5);
   }
 
   {
     Future<usize> future;
-    ASSERT(!future.isAborting());
-    ASSERT(!future.isFinished());
-    ASSERT(!future.isAborted());
+    FutureTest t;
+    future.start(t, &FutureTest::memberTest2, String("hello1"), String("hello2"));
+    future.join();
+    ASSERT(future == 42 + 6 * 2);
+  }
+
+  {
+    Future<usize> future;
+    FutureTest t;
+    future.start(t, &FutureTest::memberTest3, String("hello1"), String("hello2"), String("hello3"));
+    future.join();
+    ASSERT(future == 42 + 6 * 3);
+  }
+
+  {
+    Future<usize> future;
     FutureTest t;
     future.start(t, &FutureTest::memberTest4, String("hello1"), String("hello2"), String("hello3"), String("hello4"));
     future.join();
-    ASSERT(!future.isAborting());
-    ASSERT(future.isFinished());
-    ASSERT(!future.isAborted());
     ASSERT(future == 42 + 6 * 4);
   }
 }
