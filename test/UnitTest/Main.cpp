2a2f82f774ea792f6198325e7560967afea15e42
Colin Graf <colin.graf@sovereign-labs.com>
2020-12-30 15:24:33 +0100

    Array: Moved tests into a separate file

--- test/UnitTest/Main.cpp
+++ test/UnitTest/Main.cpp
@@ -7,7 +7,6 @@
 #include <nstd/HashSet.hpp>
 #include <nstd/List.hpp>
 #include <nstd/HashMap.hpp>
-#include <nstd/Array.hpp>
 #include <nstd/Thread.hpp>
 #include <nstd/Time.hpp>
 #include <nstd/Error.hpp>
@@ -48,6 +47,8 @@ void testRefCount();
 void testList();
 void testHashMap();
 void testFuture();
+void testArray();
+void testArrayString();
 
 void testNewDelete()
 {
@@ -78,121 +79,8 @@ void testNewDelete()
   ASSERT(destructorCalls == 24);
 }
 
-void testArray()
-{
-  // test append
-  Array<int> myArray;
-  Array<int> emptyArray;
-  myArray.swap(emptyArray);
-  ASSERT(myArray.isEmpty());
-  ASSERT(myArray.size() == 0);
-  ASSERT(myArray.append(123) == 123);
-  ASSERT(myArray.append(124) == 124);
-  ASSERT(myArray.append(125) == 125);
-  ASSERT(!myArray.isEmpty());
-  ASSERT(myArray.size() == 3);
 
-  // tets clear
-  myArray.clear();
-  ASSERT(myArray.isEmpty());
-  ASSERT(myArray.size() == 0);
-}
 
-void testArrayString()
-{
-  // test insert
-  Array<String> myArray;
-  ASSERT(myArray.isEmpty());
-  ASSERT(myArray.size() == 0);
-  ASSERT(myArray.append(_T("string1")) == _T("string1"));
-  ASSERT(myArray.append(_T("string2")) == _T("string2"));
-  ASSERT(myArray.append(_T("string3")) == _T("string3"));
-  ASSERT(!myArray.isEmpty());
-  ASSERT(myArray.size() == 3);
-
-  // test list copy constructor
-  Array<String> myArray2(myArray);
-  ASSERT(myArray2.size() == 3);
-  ASSERT(*myArray2.begin() == _T("string1"));
-  ASSERT(*(++Array<String>::Iterator(myArray2.begin())) == _T("string2"));
-  ASSERT(myArray2.back() == _T("string3"));
-
-  // test list copy operator
-  Array<String> myArray3;
-  myArray3 = myArray;
-  ASSERT(myArray3.size() == 3);
-  ASSERT(*myArray3.begin() == _T("string1"));
-  ASSERT(*(++Array<String>::Iterator(myArray3.begin())) == _T("string2"));
-  ASSERT(myArray3.back() == _T("string3"));
-
-  // test clear
-  myArray.clear();
-  ASSERT(myArray.isEmpty());
-  ASSERT(myArray.size() == 0);
-
-  // test iterator
-  String str;
-  for(int i = 0; i < 500; ++i)
-  {
-    str.printf(_T("test%d"), i);
-    myArray.append(str);
-  }
-  ASSERT(myArray.size() == 500);
-  int count = 0;
-  for(Array<String>::Iterator i = myArray.begin(), end = myArray.end(); i != end; ++i)
-  {
-    str.printf(_T("test%d"), count);
-    ASSERT(*i == str);
-    ++count;
-  }
-  ASSERT(count == 500);
-
-  // test remove
-  myArray.remove(23);
-  ASSERT(myArray.size() == 499);
-  ASSERT(myArray.remove(myArray.begin()) == myArray.begin());
-
-  // test reserve
-  myArray.clear();
-  myArray.append(_T("test1"));
-  myArray.reserve(1000);
-  ASSERT(myArray.capacity() >= 1000);
-  ASSERT(myArray.size() == 1);
-  ASSERT(myArray[0] == _T("test1"));
-
-  // test resize
-  myArray.clear();
-  for (int i = 0; i < 100; ++i)
-    myArray.append(_T("test"));
-  ASSERT(myArray.size() == 100);
-  myArray.resize(110, _T("dasda"));
-  ASSERT(myArray.size() == 110);
-  myArray.resize(90);
-  ASSERT(myArray.size() == 90);
-
-  // test front and back
-  myArray.clear();
-  myArray.append(_T("1"));
-  myArray.append(_T("2"));
-  ASSERT(myArray.front() == _T("1"));
-  ASSERT(myArray.back() == _T("2"));
-  myArray.append(_T("3"));
-  ASSERT(myArray.front() == _T("1"));
-  ASSERT(myArray.back() == _T("3"));
-
-  // test remove front and back
-  myArray.clear();
-  myArray.append(_T("1"));
-  myArray.append(_T("2"));
-  myArray.append(_T("3"));
-  myArray.removeFront();
-  ASSERT(myArray.size() == 2);
-  ASSERT(myArray.front() == _T("2"));
-  myArray.removeBack();
-  ASSERT(myArray.size() == 1);
-  ASSERT(myArray.front() == _T("2"));
-  ASSERT(myArray.back() == _T("2"));
-}
 
 int main(int argc, char* argv[])
 {
