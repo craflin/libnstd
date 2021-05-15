8f02eddd6775aeb29a19d59394294e370ad690f0
Colin Graf <colin.graf@sovereign-labs.com>
2021-05-15 18:04:29 +0200

    Math: Added randomMax

--- src/Math.cpp
+++ src/Math.cpp
@@ -6,9 +6,11 @@
 
 uint Math::random() {return rand();}
 uint Math::random(uint seed) {return srand(seed), rand();}
+const usize Math::randomMax = RAND_MAX;
 double Math::floor(double v) {return std::floor(v);}
 float Math::floor(float v) {return std::floor(v);}
 double Math::ceil(double v) {return std::ceil(v);}
 float Math::ceil(float v) {return std::ceil(v);}
 double Math::exp(double v) {return std::exp(v);}
 float Math::exp(float v) {return std::exp(v);}
+
