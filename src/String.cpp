1d91a4d4dffba3d677e13a270fa6e08d44ca7124
Colin Graf <colin.graf@sovereign-labs.com>
2025-02-09 06:51:39 +0100

    Add String::fromBase64

--- src/String.cpp
+++ src/String.cpp
@@ -391,3 +391,100 @@ String String::fromHex(const byte* data, usize size)
   }
   return result;
 }
+
+String String::fromBase64(const String& data)
+{
+    String result;
+    usize inlen = data.length();
+    if (inlen & 0x3)
+        return String();
+    result.reserve(data.length());
+    char* out = (char*)result;
+    const char* in = (const char*)data;
+    usize i;
+    usize j;
+    unsigned char c;
+    const unsigned char base64de[] = {
+        /* nul, soh, stx, etx, eot, enq, ack, bel, */
+           255, 255, 255, 255, 255, 255, 255, 255,
+    
+        /*  bs,  ht,  nl,  vt,  np,  cr,  so,  si, */
+           255, 255, 255, 255, 255, 255, 255, 255,
+    
+        /* dle, dc1, dc2, dc3, dc4, nak, syn, etb, */
+           255, 255, 255, 255, 255, 255, 255, 255,
+    
+        /* can,  em, sub, esc,  fs,  gs,  rs,  us, */
+           255, 255, 255, 255, 255, 255, 255, 255,
+    
+        /*  sp, '!', '"', '#', '$', '%', '&', ''', */
+           255, 255, 255, 255, 255, 255, 255, 255,
+    
+        /* '(', ')', '*', '+', ',', '-', '.', '/', */
+           255, 255, 255,  62, 255, 255, 255,  63,
+    
+        /* '0', '1', '2', '3', '4', '5', '6', '7', */
+            52,  53,  54,  55,  56,  57,  58,  59,
+    
+        /* '8', '9', ':', ';', '<', '=', '>', '?', */
+            60,  61, 255, 255, 255, 255, 255, 255,
+    
+        /* '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', */
+           255,   0,   1,  2,   3,   4,   5,    6,
+    
+        /* 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', */
+             7,   8,   9,  10,  11,  12,  13,  14,
+    
+        /* 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', */
+            15,  16,  17,  18,  19,  20,  21,  22,
+    
+        /* 'X', 'Y', 'Z', '[', '\', ']', '^', '_', */
+            23,  24,  25, 255, 255, 255, 255, 255,
+    
+        /* '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', */
+           255,  26,  27,  28,  29,  30,  31,  32,
+    
+        /* 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', */
+            33,  34,  35,  36,  37,  38,  39,  40,
+    
+        /* 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', */
+            41,  42,  43,  44,  45,  46,  47,  48,
+    
+        /* 'x', 'y', 'z', '{', '|', '}', '~', del, */
+            49,  50,  51//, 255, 255, 255, 255, 255
+    };
+    for (i = j = 0; i < inlen; ++i)
+    {
+        if (in[i] > 'z') 
+            return String();
+
+        c = base64de[(unsigned char)in[i]];
+        if (c == 255)
+        {
+            if (in[i] == '=')
+                break;
+
+            return String();
+        }
+
+        switch (i & 0x3)
+        {
+        case 0:
+            out[j] = (c << 2) & 0xFF;
+            break;
+        case 1:
+            out[j++] |= (c >> 4) & 0x3;
+            out[j] = (c & 0xF) << 4; 
+            break;
+        case 2:
+            out[j++] |= (c >> 2) & 0xF;
+            out[j] = (c & 0x3) << 6;
+            break;
+        case 3:
+            out[j++] |= c;
+            break;
+        }
+    }
+    result.resize(j);
+    return result;
+}
